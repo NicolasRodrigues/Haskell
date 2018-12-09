0bb818258a7d8509c4c0d356250b3ad3e4953a77
Eduardo <egbulsoni@gmail.com>
2018-12-09 20:40:00 +0000

    realizar pesquisar dica de acordo com o filtro nome

--- src/Handler/Dicas.hs
+++ src/Handler/Dicas.hs
@@ -11,19 +11,20 @@ import Text.Lucius
 import Text.Julius
 import Database.Persist.Sql
 
+
 widgetFooter :: Widget
 widgetFooter = $(whamletFile "templates/footer.hamlet")
 
 widgetMenu :: Widget
 widgetMenu = do
     sess <- lookupSession "_USR"
-    mUserId <- lookupSession "_ID"    
+    mUserId <- lookupSession "_ID"      
     $(whamletFile "templates/menu.hamlet")
 
 
 getDicasR :: Handler Html
 getDicasR = do 
-    (widget, enctype) <- generateFormPost formDicas
+    (widget, enctype) <- generateFormPost formPesquisa
     nomeDica <- runDB $ selectList [] [Asc ArtigoNome]
     foto <- mapM (\(Entity artigoid _) -> runDB $ selectFirst [PassosArtigoid ==. artigoid] []) nomeDica
     let x = zip nomeDica foto
@@ -31,47 +32,75 @@ getDicasR = do
         $(whamletFile "templates/dicas.hamlet")
         toWidget $(luciusFile "templates/menu.lucius")
         toWidget $(luciusFile "templates/footer.lucius")
-        toWidget $(luciusFile "templates/dicas.lucius")
-        addStylesheet $ (StaticR css_bootstrap_css)
 
 formDicas :: Form (Maybe (CategoriaId), Maybe (Text))
 formDicas = renderBootstrap $  pure (,)        
     <*> aopt  (selectField $ optionsPersistKey [] [] categoriaNome) "Categoria:" Nothing   
     <*> aopt  textField "Nome:" Nothing    
     
+ {-
+getPesqR :: Text -> Handler Html
+getPesqR name= do 
+    nomeDica <- runDB $ selectList [ArtigoNome ==. name][ Asc ArtigoNome]
+    foto <- mapM (\(Entity artigoid _) -> runDB $ selectFirst [PassosArtigoid ==. artigoid] []) nomeDica
+    let x = zip nomeDica foto
+    defaultLayout $ do
+         $(whamletFile "templates/dicas.hamlet")
+         toWidget $(luciusFile "templates/menu.lucius")
+         toWidget $(luciusFile "templates/footer.lucius")-}
+         
+         
+         
+getListaArtigoR :: Handler Html
+getListaArtigoR = do 
+    art <- runDB $ selectList [][ Asc ArtigoId]
+    defaultLayout $ do 
+        addStylesheet $ (StaticR css_bootstrap_css)        
+        $(whamletFile "templates/listaartigo.hamlet")
+        toWidget $(luciusFile "templates/menu.lucius")
+        toWidget $(luciusFile "templates/footer.lucius")           
         
-getPesq2R :: Handler Html
-getPesq2R = do 
-    ((res,_),_) <- runFormPost formDicas
+getApagarArtigoR :: ArtigoId ->  Handler Html
+getApagarArtigoR aid = do  
+    runDB $ deleteCascade  aid
+    redirect ListaArtigoR            
+        
+        
+data Pesquisa = Pesquisa
+    { pesquisa          :: Text
+    }
+    
+formPesquisa :: Form Pesquisa
+formPesquisa = renderBootstrap $ Pesquisa
+        <$> areq textField FieldSettings{fsId=Just "campo1",
+                           fsLabel="Nome",
+                           fsTooltip= Nothing,
+                           fsName= Nothing,
+                           fsAttrs=[("class","form-control"),("placeholder","Digite sua pesquisa"),("style","width:70%")]} Nothing        
+                           
+
+toTexto :: Pesquisa -> Text
+toTexto (Pesquisa x) = x
+
+postPesqArtigoR :: Handler Html
+postPesqArtigoR = do 
+    ((res, _), _) <- runFormPost formPesquisa
     case res of
-        FormSuccess (cat, nome) -> do 
-            case nome of 
-                Just name -> do            
-                 nomeDica <- runDB $ selectList [ArtigoNome ==. name][ Asc ArtigoNome]
-                 foto <- mapM (\(Entity artigoid _) -> runDB $ selectFirst [PassosArtigoid ==. artigoid] []) nomeDica
-                 let x = zip nomeDica foto
-                 defaultLayout $ do
-                     $(whamletFile "templates/dicas.hamlet")
-                     toWidget $(luciusFile "templates/menu.lucius")
-                     toWidget $(luciusFile "templates/footer.lucius")
-                     toWidget $(luciusFile "templates/dicas.lucius")
-                     addStylesheet $ (StaticR css_bootstrap_css)
-                Nothing -> do
-                 case cat of 
-                     Just catego -> do            
-                      nomeDica <- runDB $ selectList [ArtigoCategoriaid ==. catego][ Asc ArtigoCategoriaid]
-                      foto <- mapM (\(Entity artigoid _) -> runDB $ selectFirst [PassosArtigoid ==. artigoid] []) nomeDica
-                      let x = zip nomeDica foto
-                      defaultLayout $ do
-                          $(whamletFile "templates/dicas.hamlet")
-                          toWidget $(luciusFile "templates/menu.lucius")
-                          toWidget $(luciusFile "templates/footer.lucius")   
-                          toWidget $(luciusFile "templates/dicas.lucius")
-                          addStylesheet $ (StaticR css_bootstrap_css)
-                     Nothing -> do
-                          setMessage [shamlet|
-                              <div class="alert alert-danger">
-                                  Não temos informacoes para esta pesquisa
-                          |]
-                          redirect $ UsuarioR 
- 
+        FormSuccess pesquisar -> do 
+            let nome = toTexto(pesquisar)
+            redirect (BuscarArtigoR nome)
+        _ -> do
+            setMessage $ [shamlet| Dados invalidos! |] 
+            redirect HomeR                         
+        
+    
+getBuscarArtigoR :: Text -> Handler Html
+getBuscarArtigoR art = do
+    (widget, enctype) <- generateFormPost formPesquisa
+    nomeDica <- runDB $ selectList ([Filter ArtigoNome (Left $ "%"++ art ++"%") (BackendSpecificFilter "ILIKE")])[Asc ArtigoNome]    
+    foto <- mapM (\(Entity artigoid _) -> runDB $ selectFirst [PassosArtigoid ==. artigoid] []) nomeDica
+    let x = zip nomeDica foto
+    defaultLayout $ do
+         $(whamletFile "templates/dicas.hamlet")
+         toWidget $(luciusFile "templates/menu.lucius")
+         toWidget $(luciusFile "templates/footer.lucius")                              
